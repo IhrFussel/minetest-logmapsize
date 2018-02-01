0de8fa42910fbd050042b1348f93b259da1d9636
IhrFussel <angelofberlin91@gmail.com>
2018-02-01 22:10:10 +0100

    Minor fixes

--- logmapsize.sh
+++ logmapsize.sh
@@ -3,13 +3,13 @@ interval=1800 ### Log interval
 logfile="" ### Define file to log the sizes
 
 if [[ $path = "" ]]; then
-echo "Open the script and enter the path to the map.sqlite file"
+echo "ERROR: Open the script and enter the path to the map.sqlite file"
 exit
 fi
 
 while true
 do
 getsize=$(du $path | grep -Eo "^[0-9]{1,}")
-echo "$(date) -> $(($getsize/1024)) MB" >> $file
+echo "$(date) -> $(($getsize/1024)) MB" >> $logfile
 sleep $interval
 done
